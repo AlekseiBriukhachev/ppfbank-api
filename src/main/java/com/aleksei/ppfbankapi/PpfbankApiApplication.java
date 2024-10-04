6cf7cf4b8b394f3292588071169fa8fdbb6abe0a
Aleksei Briukhachev <aleksei.develop@gmail.com>
2024-10-04 08:21:36 +0200

    Initial commit

---
+++ src/main/java/com/aleksei/ppfbankapi/PpfbankApiApplication.java
@@ -0,0 +1,13 @@
+package com.aleksei.ppfbankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpfbankApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpfbankApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aleksei/ppfbankapi/PpfbankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksei.ppfbankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpfbankApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
